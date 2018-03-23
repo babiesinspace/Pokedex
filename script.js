b96a11ba629dbdab512a6e43f3434d74a45ce612
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-23 14:55:18 -0400

    create a bunch of chained callbacks to only push my pokemon into my slave array once the api call has completed and created a new pokemon object for each monster..thing.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+class Pokemon {
+  constructor(args) {
+    this.name = args.name
+    this.image = args.image
+    this.pokeNum = args.pokeNum
+    this.types = args.types
+    this.height = args.height
+    this.weight = args.weight
+    this.abilities = args.abilities
+    this.stats = args.stats
+  }
+}
+
 let pokeHash = {
   name: "",
   image: "",
@@ -9,23 +22,27 @@ let pokeHash = {
   stats: []
 }
 
-
-$.ajax({
-  url: "https://pokeapi.co/api/v2/pokemon/4",
-  success: function(response){
-    pokeHash.name = response.name
-    pokeHash.image = response.sprites.front_default
-    pokeHash.pokeNum = response.id
-    pokeHash.types = response.types.map(function(type){return type.type.name})
-    pokeHash.height = response.height
-    pokeHash.weight = response.weight
-    response.abilities.map(function(power){pokeHash.abilities.push({name: power.ability.name, isHidden: power.is_hidden})})
-    response.stats.map(function(statistic){setStats(statistic)})
-  },
-  error: function(error){
-    console.log(error)
-  }
-})
+function catchPokemon(pokeNameOrIdNumber){
+  return $.ajax({
+    url: "https://pokeapi.co/api/v2/pokemon/" + pokeNameOrIdNumber,
+    success: function(response){
+      pokeHash.name = response.name
+      pokeHash.image = response.sprites.front_default
+      pokeHash.pokeNum = response.id
+      pokeHash.types = response.types.map(function(type){return type.type.name})
+      pokeHash.height = response.height
+      pokeHash.weight = response.weight
+      response.abilities.map(function(power){pokeHash.abilities.push({name: power.ability.name, isHidden: power.is_hidden})})
+      response.stats.map(function(statistic){setStats(statistic)})
+      let pokemon = new Pokemon(pokeHash)
+      console.log(pokemon)
+      return pokemon
+    },
+    error: function(error){
+      console.log(error)
+    }
+  })
+}
 
 function setStats(statistic) {
   let obj = {}
@@ -34,6 +51,17 @@ function setStats(statistic) {
   obj[name] = baseValue
   pokeHash.stats.push(obj)
 }
+
+let charmander = {}
+let raichu = {}
+let jolteon = {}
+let mySlaves = []
+
+catchPokemon("4").done(function(result){charmander = result; mySlaves.push(charmander)}).done(catchPokemon("26").done(function(result){raichu = result; mySlaves.push(raichu)})).done(catchPokemon("135").done(function(result){jolteon = result; mySlaves.push(jolteon)}))
+
+// console.log(mySlaves)
+
+// console.log(mySlaves)
 
 //My Pokemon:
 //Charmander #5
